140576d59abfa532dcaf9716dd5175432a7c5e26
muscleboy <1076146831@qq.com>
2019-09-22 20:57:25 +0800

    订单分页完成

--- src/main/java/xyz/bugcoder/controller/OrderController.java
+++ src/main/java/xyz/bugcoder/controller/OrderController.java
@@ -1,7 +1,10 @@
 package xyz.bugcoder.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.bugcoder.bean.Order;
 import xyz.bugcoder.service.OrderService;
@@ -23,9 +26,14 @@ public class OrderController {
     OrderService orderService;
 
     @GetMapping("/orders")
-    public List<Order> listOrder(){
+    public PageInfo<Order> listOrder(@RequestParam(value = "start", defaultValue = "0") int start,
+                                     @RequestParam(value = "size", defaultValue = "5") int size){
 
-        return orderService.list();
+        start = start < 0 ? 0 : start;
+        PageHelper.startPage(start, size, "id desc");
+        List<Order> os = orderService.list();
+        PageInfo<Order> page = new PageInfo<>(os, 5);
+        return page;
     }
 
 }
